917f3a99dad6fef9c2fe5d6f7962869162a1195f
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-04-15 17:41:39 -0700

    integrates wouter for simple client-side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { Link, Route, Switch } from 'wouter';
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* anything outside of the switch will render on all "routes" */}
+      <Switch>
+        <Route path="/routine">
+          <h1 className="text-4xl font-semibold">Good morning 🌞</h1>
+        </Route>
+
+        <Route path="/">
+          {/* TODO: navbar */}
+          <p>home route / marketing site</p>
+        </Route>
+
+        {/* Default route in a switch is the 404 route */}
+        <Route>
+          <div className="flex flex-col justify-normal gap-8">
+            <p>404: Looks like we're off the path!</p>
+            <Link
+              href="/routine"
+              className="inline-block rounded-md bg-blue-600 text-white"
+            >
+              Back to my Routine
+            </Link>
+          </div>
+        </Route>
+      </Switch>
     </>
   );
 }
