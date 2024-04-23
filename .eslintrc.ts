a53dfd6e783f8f7280983e9905aabbfa3353ac7b
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-04-23 16:44:49 -0700

    update eslint config for production

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,13 +1,22 @@
-module.exports = {
+export default {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: './',
+  },
   plugins: ['@typescript-eslint', 'react', 'react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
