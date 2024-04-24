0050886aff072927b27c29e4fb2bacb913b933bf
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-04-24 16:21:12 -0700

    sets up Jotai store / Provider
    - usage similar to React Context; enables using state anywhere down the tree
    - enables direct mutation of the store (outside of React), that subsequently triggers an appropriate rerender

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import './index.css';
+import { Provider as JotaiProvider, createStore } from 'jotai';
+import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+
+// similar to React context; the jotaiStore can be used throughout the app
+// as long as the components are children of the Provider
+export const jotaiStore = createStore();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* The Jotai Provider ensures that updates to the store trigger re-renders */}
+    <JotaiProvider store={jotaiStore}>
     <App />
+    </JotaiProvider>
   </React.StrictMode>,
 );
