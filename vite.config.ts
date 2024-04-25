d8bd5608f697b670ce34cd6a476749c5241975aa
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-04-25 10:22:00 -0700

    adds favicon assets and code entries

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,30 @@ export default defineConfig({
         short_name: 'Routine',
         description: 'A simple tool to help keep your mornings on track.',
         theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'favicon/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
       },
       devOptions: {
         enabled: true,
