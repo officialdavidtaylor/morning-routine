bb5b15f53931008dbccd41556c513f27c519c732
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-04-25 10:23:59 -0700

    introduces Google Fonts
    - import scripts in `index.html`
    - theme extension in tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
 import tailwindcss_forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Outfit', ...defaultTheme.fontFamily.sans],
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [tailwindcss_forms],
 } satisfies Config;
