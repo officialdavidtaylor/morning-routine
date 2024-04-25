c361aabab512da80dcc9a317ca85e0ef49b8a5e9
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-04-25 08:49:54 -0700

    enables pwa behavior
    - installs `vite-plugin-pwa` and adds baseline configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      base: '/routine',
+      manifest: {
+        name: 'Morning Routine Helper',
+        short_name: 'Routine',
+        description: 'A simple tool to help keep your mornings on track.',
+        theme_color: '#ffffff',
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
